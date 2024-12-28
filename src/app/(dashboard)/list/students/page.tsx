c21889d579156dfc153e5f6ba7e14a5ed6cf21dc
samya-roy <samyaroy999@gmail.com>
2024-12-28 14:11:37 +0600

    StudentListPage design completed

--- src/app/(dashboard)/list/students/page.tsx
+++ src/app/(dashboard)/list/students/page.tsx
@@ -1,36 +1,46 @@
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { teachersData } from "@/lib/data";
-import { Link } from "lucide-react";
+import { studentsData } from "@/lib/data";
 import Image from "next/image";
-import React from "react";
+import Link from "next/link";
 
-type Teacher = {
+type Student = {
   id: number;
-  teacherId: string;
+  studentId: string;
   name: string;
   email?: string;
   photo: string;
-  phone: string;
-  subjects: string[];
-  classes: string[];
+  phone?: string;
+  grade: number;
+  class: string;
   address: string;
 };
 
 const columns = [
   {
-    header: "Title",
-    accessor: "title",
+    header: "Info",
+    accessor: "info",
   },
   {
-    header: "Class",
-    accessor: "class",
-  },
-  {
-    header: "Date",
-    accessor: "date",
+    header: "Student ID",
+    accessor: "studentId",
     className: "hidden md:table-cell",
+  },
+  {
+    header: "Grade",
+    accessor: "grade",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Phone",
+    accessor: "phone",
+    className: "hidden lg:table-cell",
+  },
+  {
+    header: "Address",
+    accessor: "address",
+    className: "hidden lg:table-cell",
   },
   {
     header: "Actions",
@@ -38,9 +48,12 @@ const columns = [
   },
 ];
 
-const TeacherListPage = () => {
-  const renderRow = (item: Teacher) => (
-    <tr key={item.id} className="border-b text-sm hover:bg-lamaPurpleLight">
+const StudentListPage = () => {
+  const renderRow = (item: Student) => (
+    <tr
+      key={item.id}
+      className="border-b border-gray-200 text-sm hover:bg-lamaPurpleLight"
+    >
       <td className="flex items-center gap-4 p-4">
         <Image
           src={item.photo}
@@ -51,12 +64,11 @@ const TeacherListPage = () => {
         />
         <div className="flex flex-col">
           <h3 className="font-semibold">{item.name}</h3>
-          <p className="text-xs text-gray-500">{item?.email}</p>
+          <p className="text-xs text-gray-500">{item.class}</p>
         </div>
       </td>
-      <td className="hidden md:table-cell">{item.teacherId}</td>
-      <td className="hidden md:table-cell">{item.subjects.join(",")}</td>
-      <td className="hidden md:table-cell">{item.classes.join(",")}</td>
+      <td className="hidden md:table-cell">{item.studentId}</td>
+      <td className="hidden md:table-cell">{item.grade}</td>
       <td className="hidden md:table-cell">{item.phone}</td>
       <td className="hidden md:table-cell">{item.address}</td>
       <td>
@@ -66,6 +78,12 @@ const TeacherListPage = () => {
               <Image src="/view.png" alt="" width={16} height={16} />
             </button>
           </Link>
+          {/* {role === "admin" && (
+            // <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
+            //   <Image src="/delete.png" alt="" width={16} height={16} />
+            // </button>
+            <FormModal table="student" type="delete" id={item.id} />
+          )} */}
         </div>
       </td>
     </tr>
@@ -75,30 +93,29 @@ const TeacherListPage = () => {
     <div className=" p-4 rounded-md flex-1 m-4 mt-0">
       {/* TOP */}
       <div className="flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold">All Teachers</h1>
+        <h1 className="hidden md:block text-lg font-semibold">All Students</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
-          <div className="flex items-center gap-4 self-end">
+          <div className="flex items-center  gap-4 self-end">
             <button className="w-8 h-8 flex dark:bg-white items-center justify-center rounded-full bg-lamaYellow">
               <Image src="/filter.png" alt="" width={14} height={14} />
             </button>
-            <button className="w-8 h-8 flex items-center dark:bg-white justify-center rounded-full bg-lamaYellow">
+            <button className="w-8 h-8 flex dark:bg-white items-center justify-center rounded-full bg-lamaYellow">
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
 
-            <button className="w-8 h-8 flex items-center dark:bg-white justify-center rounded-full bg-lamaYellow">
+            <button className="w-8 h-8 dark:bg-white flex items-center justify-center rounded-full bg-lamaYellow">
               <Image src="/plus.png" alt="" width={14} height={14} />
             </button>
-            {/* <FormModal table="teacher" type="create" /> */}
           </div>
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={teachersData} />
+      <Table columns={columns} renderRow={renderRow} data={studentsData} />
       {/* PAGINATION */}
       <Pagination />
     </div>
   );
 };
 
-export default TeacherListPage;
+export default StudentListPage;
