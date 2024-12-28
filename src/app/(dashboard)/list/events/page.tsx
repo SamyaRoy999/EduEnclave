a2bc4339e5b2aa9c884f4d06fb450b408e411bcc
samya-roy <samyaroy999@gmail.com>
2024-12-28 21:57:35 +0600

    dashboard list all design added

--- src/app/(dashboard)/list/events/page.tsx
+++ src/app/(dashboard)/list/events/page.tsx
@@ -2,14 +2,16 @@ import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
 import { Button } from "@/components/ui/button";
-import { announcementsData, role } from "@/lib/data";
+import { eventsData, role } from "@/lib/data";
 import Image from "next/image";
 
-type Announcement = {
+type Event = {
   id: number;
   title: string;
   class: string;
   date: string;
+  startTime: string;
+  endTime: string;
 };
 
 const columns = [
@@ -27,13 +29,23 @@ const columns = [
     className: "hidden md:table-cell",
   },
   {
+    header: "Start Time",
+    accessor: "startTime",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "End Time",
+    accessor: "endTime",
+    className: "hidden md:table-cell",
+  },
+  {
     header: "Actions",
     accessor: "action",
   },
 ];
 
-const AnnouncementListPage = () => {
-  const renderRow = (item: Announcement) => (
+const EventListPage = () => {
+  const renderRow = (item: Event) => (
     <tr
       key={item.id}
       className="border-b border-gray-200  text-sm hover:bg-lamaPurpleLight"
@@ -41,6 +53,8 @@ const AnnouncementListPage = () => {
       <td className="flex items-center gap-4 p-4">{item.title}</td>
       <td>{item.class}</td>
       <td className="hidden md:table-cell">{item.date}</td>
+      <td className="hidden md:table-cell">{item.startTime}</td>
+      <td className="hidden md:table-cell">{item.endTime}</td>
       <td>
         <div className="flex items-center gap-2">
           {role === "admin" && (
@@ -54,30 +68,28 @@ const AnnouncementListPage = () => {
   );
 
   return (
-    <div className=" p-4 rounded-md flex-1 m-4 mt-0">
+    <div className="p-4 rounded-md flex-1 m-4 mt-0">
       {/* TOP */}
       <div className="flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold">
-          All Announcements
-        </h1>
+        <h1 className="hidden md:block text-lg font-semibold">All Events</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
-            <button className="w-8 dark:bg-white h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+            <button className="w-8 h-8 flex dark:bg-white  items-center justify-center rounded-full bg-lamaYellow">
               <Image src="/filter.png" alt="" width={14} height={14} />
             </button>
-            <button className="w-8 dark:bg-white h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+            <button className="w-8 h-8 flex  dark:bg-white items-center justify-center rounded-full bg-lamaYellow">
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
           </div>
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={announcementsData} />
+      <Table columns={columns} renderRow={renderRow} data={eventsData} />
       {/* PAGINATION */}
       <Pagination />
     </div>
   );
 };
 
-export default AnnouncementListPage;
+export default EventListPage;
